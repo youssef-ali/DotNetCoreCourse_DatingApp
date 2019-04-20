ab607e4abac4a0192d886d308224f472f23ae6bf
youssef.ali50 <youssef.ali50@users.noreply.github.com>
2019-04-21 00:50:06 +0400

    Add AuthRepository and Auth Controller

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -34,6 +34,7 @@ namespace DatingApp.API
             });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
